f62c456b7f7a33951e3e2006e6853d412da3b1ee
ebattleGWT <129992226+ebattleGWT@users.noreply.github.com>
2023-05-05 09:42:45 -0400

    Update prevent-po-box.js

--- prevent-po-box.js
+++ prevent-po-box.js
@@ -1,23 +1,22 @@
-var shippingStep = 'step=contact_information';
-var isShippingStep = window.location.href.indexOf(shippingStep) !== -1;
-if (isShippingStep){
+ var shippingStep = 'step=contact_information';
+        var isShippingStep = window.location.href.indexOf(shippingStep) !== -1;
+        if (isShippingStep){
   document.addEventListener('DOMContentLoaded', function() {
     var continueButton = document.getElementById('continue_button');
     if (continueButton) {
       continueButton.addEventListener('click', function(event) {
         var addressField1 = document.querySelector('#checkout_shipping_address_address1');
         var addressField2 = document.querySelector('#checkout_shipping_address_address2')
-        if ((addressField1 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField1.value)) || 
-          (addressField2 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField2.value))) {
+        if ((addressField1 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField1.value)) || 
+          (addressField2 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField2.value))) {
           alert('No PO Boxes allowed. Please update your mailing address.');
           event.preventDefault();
         }
       });
     }
-  });
-}
+  });}
 
-var billingStep = 'step=payment_method';
+        var billingStep = 'step=payment_method';
 var isBillingStep = window.location.href.indexOf(billingStep) !== -1;
 if (isBillingStep) {
   document.addEventListener('DOMContentLoaded', function() {
@@ -26,8 +25,8 @@ if (isBillingStep) {
       continueButton.addEventListener('click', function(event) {
         var addressField1 = document.querySelector('#checkout_billing_address_address1');
         var addressField2 = document.querySelector('#checkout_billing_address_address2')
-        if ((addressField1 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField1.value)) || 
-          (addressField2 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField2.value))) {
+        if ((addressField1 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField1.value)) || 
+          (addressField2 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField2.value))) {
           alert('No PO Boxes allowed. Please update your billing address.');
           event.preventDefault();
         }
