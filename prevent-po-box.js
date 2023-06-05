fd01a0901640537309f7c23b9dfa152ff9619080
ebattleGWT <129992226+ebattleGWT@users.noreply.github.com>
2023-06-05 10:07:00 -0400

    Update prevent-po-box.js
    Corrected the code and added more expressions to block.

--- prevent-po-box.js
+++ prevent-po-box.js
@@ -1,32 +1,35 @@
- var shippingStep = 'step=contact_information';
-        var isShippingStep = window.location.href.indexOf(shippingStep) !== -1;
-        if (isShippingStep){
+var shippingStep = 'step=contact_information';
+var isShippingStep = window.location.href.indexOf(shippingStep) !== -1;
+if (isShippingStep){
   document.addEventListener('DOMContentLoaded', function() {
     var continueButton = document.getElementById('continue_button');
     if (continueButton) {
       continueButton.addEventListener('click', function(event) {
         var addressField1 = document.querySelector('#checkout_shipping_address_address1');
-        var addressField2 = document.querySelector('#checkout_shipping_address_address2')
-        if ((addressField1 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField1.value)) || 
-          (addressField2 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField2.value))) {
+        var addressField2 = document.querySelector('#checkout_shipping_address_address2');
+        var poBoxRegex = /P\s*\.?\s*O\s*\.?\s*BOX|P\s*\.?\s*O|PO\s*\.?\s*BOX|PO\s*BOX|POST\s*OFFICE\s*BOX/i;
+        if ((addressField1 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField1.value)) || 
+          (addressField2 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField2.value))) {
           alert('No PO Boxes allowed. Please update your mailing address.');
           event.preventDefault();
         }
       });
     }
-  });}
+  });
+}
 
-        var billingStep = 'step=payment_method';
+var billingStep = 'step=payment_method';
 var isBillingStep = window.location.href.indexOf(billingStep) !== -1;
 if (isBillingStep) {
   document.addEventListener('DOMContentLoaded', function() {
     var continueButton = document.getElementById('continue_button');
     if (continueButton) {
       continueButton.addEventListener('click', function(event) {
-        var addressField1 = document.querySelector('#checkout_billing_address_address1');
-        var addressField2 = document.querySelector('#checkout_billing_address_address2')
-        if ((addressField1 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField1.value)) || 
-          (addressField2 && /PO\s?BOX|P\.?O\.?\s?BOX|POB/i.test(addressField2.value))) {
+        var addressField3 = document.querySelector('#checkout_billing_address_address1');
+        var addressField4 = document.querySelector('#checkout_billing_address_address2');
+        var poBoxRegex2 = /P\s*\.?\s*O\s*\.?\s*BOX|P\s*\.?\s*O|PO\s*\.?\s*BOX|PO\s*BOX|POST\s*OFFICE\s*BOX/i;
+        if ((addressField3 && poBoxRegex2.test(addressField3.value)) || 
+          (addressField4 && poBoxRegex2.test(addressField4.value))) {
           alert('No PO Boxes allowed. Please update your billing address.');
           event.preventDefault();
         }
