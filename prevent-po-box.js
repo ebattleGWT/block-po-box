fc6f755e65a33b254030ee2fe2371591b35df4cd
ebattleGWT <129992226+ebattleGWT@users.noreply.github.com>
2023-06-21 11:03:22 -0400

    Update prevent-po-box.js

--- prevent-po-box.js
+++ prevent-po-box.js
@@ -1,14 +1,17 @@
-  var shippingStep = 'step=contact_information';
+var shippingStep = 'step=contact_information';
 var isShippingStep = window.location.href.indexOf(shippingStep) !== -1;
 if (isShippingStep){
   document.addEventListener('DOMContentLoaded', function() {
     var continueButton = document.getElementById('continue_button');
     if (continueButton) {
       continueButton.addEventListener('click', function(event) {
+        var country = document.body.getAttribute('data-checkout-country');
         var addressField1 = document.querySelector('#checkout_shipping_address_address1');
         var addressField2 = document.querySelector('#checkout_shipping_address_address2');
         var poBoxRegex = /(P(\.|O(\.|st)?)?(\s+B(ox)?)?)\s+\d+/i;
-        if ((addressField1 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField1.value)) || 
+        if (country !== "US") {
+          // Implement your custom checks for non-US addresses here
+        } else if ((addressField1 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField1.value)) || 
           (addressField2 && /(P\.?\s?O\.?|PO|Box)\s?\d+/i.test(addressField2.value))) {
           alert('No PO Boxes allowed. Please update your mailing address.');
           event.preventDefault();
@@ -24,10 +27,13 @@ if (isBillingStep) {
     var continueButton = document.getElementById('continue_button');
     if (continueButton) {
       continueButton.addEventListener('click', function(event) {
+        var country = document.body.getAttribute('data-checkout-country');
         var addressField3 = document.querySelector('#checkout_billing_address_address1');
         var addressField4 = document.querySelector('#checkout_billing_address_address2');
         var poBoxRegex2 = /(P(\.|O(\.|st)?)?(\s+B(ox)?)?)\s+\d+/i;
-        if ((addressField3 && poBoxRegex2.test(addressField3.value)) || 
+        if (country !== "US") {
+          // Implement your custom checks for non-US addresses here
+        } else if ((addressField3 && poBoxRegex2.test(addressField3.value)) || 
           (addressField4 && poBoxRegex2.test(addressField4.value))) {
           alert('No PO Boxes allowed. Please update your billing address.');
           event.preventDefault();
